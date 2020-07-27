5342852879988edf8a0fffd30aab5122cd395062
Вадим <vadMoroz13@yandex.ru>
2020-07-28 00:35:36 +0300

    Home2

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -3,20 +3,23 @@ const calc = (data) => {
 //console.log(+!data.type +1);
 const type = +!data.type +1
 const count =  (+!data.type +1 === 1) ? 10000 : 150000;
-const diamCount = data.diamCount[0] || 1;
-const diam = data.diam[0] || 1;
+const diamCount = data.diamCount[0] || 0;
+const diam = data.diam[0] || 0;
+
  const bottom = (data.bottom) ? 1000 : 0;
     let sum = count;
     if(type === 1){
-      sum = count * data.diam[0] * diamCount + bottom;
-    //sum = count + (count * data.diam[0]* -diamCount) //* (count * diamCount) + bottom;
-console.log(data);
+        console.log((count * data.diam[0]));
+        console.log(count * diamCount );
+      sum = count + (count * diam) +(count * diamCount ) + bottom;
+    
+//console.log(data);
 
     }else {
-        sum = count * diam * diamCount + bottom * 2;
+        sum = count + (count * diam * 2) +(count * diamCount * 2) + bottom *2;
        console.log(data.diam[0]);  
     }
 calcResult.value = sum;
-console.log(data);
+//console.log(data);
 }
 export default calc
